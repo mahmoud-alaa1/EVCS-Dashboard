ad3e3bca119743b2a371be920da6ffef7674f29a
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-30 20:23:37 +0300

    Style, Table

--- src/featuers/Users/UsersTable.tsx
+++ src/featuers/Users/UsersTable.tsx
@@ -1,12 +1,12 @@
 import SearchInput from "../../ui/SearchInput";
 import Table from "../../ui/Table";
-
+import "./UsersTableStyle.css";
 export default function UsersTable({ className }: { className?: string }) {
   return (
     <div className={className}>
       <SearchInput placeholder="Search a customer" className="w-full mb-3" />
-      <Table className="text-left rounded-xl  border border-[#C8C8C8] ">
-        <Table.Header className=" bg-[#F0F7EA] text-xs font-[500]">
+      <Table className="text-left">
+        <Table.Header className="  text-xs font-[500]">
           <Table.Heading className="w-[31%] px-5 py-3">Name</Table.Heading>
           <Table.Heading className="w-[28.8%]  px-5 py-3">
             Location
@@ -21,9 +21,3 @@ export default function UsersTable({ className }: { className?: string }) {
     </div>
   );
 }
-// width: Fill (271px)px;
-// height: Fixed (38px)px;
-// padding: 0px 20px 0px 20px;
-// gap: 4px;
-// border: 0px 0px 1px 0px;
-// opacity: 0px;
