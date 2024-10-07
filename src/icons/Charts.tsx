58287fd70bdf434703b2f884a4ffa3b008931c21
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-07 06:43:28 +0300

    Icons change
    I have changed charts and user icon
    I have added new icon Van

--- src/icons/Charts.tsx
+++ src/icons/Charts.tsx
@@ -1,42 +1,15 @@
-export default function Charts({ ...props }: React.SVGAttributes<SVGElement>) {
+export default function Charts() {
   return (
     <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      {...props}
     >
       <path
-        d="M8 10L8 16"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M12 12V16"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M16 8V16"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <rect
-        x="3"
-        y="4"
-        width="18"
-        height="16"
-        rx="2"
-        stroke="currentColor"
-        strokeWidth="2"
+        d="M6 2.5C6 1.6875 6.65625 1 7.5 1H8.5C9.3125 1 10 1.6875 10 2.5V13.5C10 14.3438 9.3125 15 8.5 15H7.5C6.65625 15 6 14.3438 6 13.5V2.5ZM1 8.5C1 7.6875 1.65625 7 2.5 7H3.5C4.3125 7 5 7.6875 5 8.5V13.5C5 14.3438 4.3125 15 3.5 15H2.5C1.65625 15 1 14.3438 1 13.5V8.5ZM12.5 3H13.5C14.3125 3 15 3.6875 15 4.5V13.5C15 14.3438 14.3125 15 13.5 15H12.5C11.6562 15 11 14.3438 11 13.5V4.5C11 3.6875 11.6562 3 12.5 3Z"
+        fill="currentColor"
       />
     </svg>
   );
